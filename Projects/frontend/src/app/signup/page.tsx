fbc46e7e88c4ab7a0b575ea33fb0c6ae155fa20b
Akshat <70918043+ftaxats@users.noreply.github.com>
2024-10-01 02:12:47 +0530

    Update page.tsx

--- Projects/frontend/src/app/signup/page.tsx
+++ Projects/frontend/src/app/signup/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import axios from 'axios';
-import Link from 'next/link'
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { appName, serverURL } from '@/utils/utils';
 import { ToastContainer, toast } from 'react-toastify';
+import Head from 'next/head';
 
-export default function Home() {
+export default function SignUp() {
     useEffect(() => {
         if (typeof window !== 'undefined') {
             if (localStorage.getItem("token")) {
@@ -19,16 +20,63 @@ export default function Home() {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
+    const [emailError, setEmailError] = useState<string>("");
+    const [passwordError, setPasswordError] = useState<string>("");
+    const [nameError, setNameError] = useState<string>("");
+
     const [verificationCodeSent, setVerificationCodeSent] = useState<boolean>(false);
     const [verificationCode, setVerificationCode] = useState<string>("");
 
     const [loading, setLoading] = useState<boolean>(false);
 
+    // Validation Functions
+    const validateName = (name: string) => {
+        return name.trim().length > 0;
+    };
+
+    const validateEmail = (email: string) => {
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|net|org)$/;
+        return emailRegex.test(email);
+    };
+
+    const validatePassword = (password: string) => {
+        const hasUpperCase = /[A-Z]/.test(password);
+        const hasLowerCase = /[a-z]/.test(password);
+        const hasNumbers = /\d/.test(password);
+        const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+        return password.length >= 8 && hasUpperCase && hasLowerCase && hasNumbers && hasSpecialChar;
+    };
+
+    // Handle Sending Verification Code
     const sendVerificationCode = async () => {
         setLoading(true);
-        if (email == "" || name == "" || password == "") {
+        
+        // Validate Inputs
+        let valid = true;
+
+        if (!validateName(name)) {
+            setNameError("Full name is required.");
+            valid = false;
+        } else {
+            setNameError("");
+        }
+
+        if (!validateEmail(email)) {
+            setEmailError("Please enter a valid email address.");
+            valid = false;
+        } else {
+            setEmailError("");
+        }
+
+        if (!validatePassword(password)) {
+            setPasswordError("Password must be at least 8 characters long and include uppercase, lowercase, number, and special character.");
+            valid = false;
+        } else {
+            setPasswordError("");
+        }
+
+        if (!valid) {
             setLoading(false);
-            toast.error("Please fill out all fields!");
             return;
         }
 
@@ -44,26 +92,29 @@ export default function Home() {
             }
         };
 
-        axios(config)
-            .then((response) => {
-                toast.success("Verification Code Sent!");
-                setVerificationCodeSent(true);
-                setLoading(false);
-            })
-            .catch((error) => {
+        try {
+            const response = await axios(config);
+            toast.success("Verification Code Sent!");
+            setVerificationCodeSent(true);
+        } catch (error: any) {
+            if (error.response && error.response.data) {
+                toast.error(error.response.data);
+            } else {
                 toast.error("Something went wrong! Please try again later.");
-                setLoading(false);
-            });
+            }
+        } finally {
+            setLoading(false);
+        }
     }
 
+    // Handle Email Verification
     const verifyEmail = async () => {
-        if (email == "" || name == "" || password == "") {
-            toast.error("Please fill out all fields!");
-            return;
-        }
+        setLoading(true);
 
-        if (verificationCode == "") {
+        // Validate Verification Code
+        if (verificationCode.trim() === "") {
             toast.error("Please enter the verification code!");
+            setLoading(false);
             return;
         }
 
@@ -80,21 +131,24 @@ export default function Home() {
             }
         };
 
-        axios(config)
-            .then((response) => {
-                toast.success("Email verified!");
-                signup();
-            })
-            .catch((error) => {
+        try {
+            const response = await axios(config);
+            toast.success("Email verified!");
+            signup();
+        } catch (error: any) {
+            if (error.response && error.response.data) {
+                toast.error(error.response.data);
+            } else {
                 toast.error("Something went wrong! Please try again later.");
-            });
+            }
+        } finally {
+            setLoading(false);
+        }
     }
 
+    // Handle Sign Up
     const signup = async () => {
-        if (email == "" || name == "" || password == "") {
-            toast.error("Please fill out all fields!");
-            return;
-        }
+        setLoading(true);
 
         const config = {
             method: "POST",
@@ -110,51 +164,124 @@ export default function Home() {
             }
         };
 
-        axios(config)
-            .then((response) => {
-                toast.success("Account created!");
-                setTimeout(() => {
-                    window.location.href = "/login";
-                }, 1000);
-            })
-            .catch((error) => {
+        try {
+            const response = await axios(config);
+            toast.success("Account created!");
+            setTimeout(() => {
+                window.location.href = "/login";
+            }, 1500);
+        } catch (error: any) {
+            if (error.response && error.response.data) {
+                toast.error(error.response.data);
+            } else {
                 toast.error("Something went wrong!");
-            });
+            }
+        } finally {
+            setLoading(false);
+        }
     }
 
     return (
-        <main className="w-screen h-screen bg-base-100 flex p-2 overflow-hidden">
-            <div className='flex flex-col text-white p-10 max-w-[30vw] bg-gradient-to-b to-purple-400 via-violet-500 from-indigo-600 h-full rounded-md'>
-                <Link href={"/"}><p className="mb-10">🤖 {appName} 📝</p></Link>
-                <p className="text-2xl font-semibold mb-2">
-                    {appName} - AI Powered Exam Sheet Evaluator
-                </p>
-                <p className="opacity-70">Seamless Access, Effortless Evaluation: Welcome to EvaluateAI, Where Innovation Meets Intelligent Grading.</p>
-            </div>
-            <div className="animate-fade-in-bottom flex flex-col w-full h-full ml-2 rounded-md p-10">
-                <p className="font-bold text-xl mb-3">Sign Up</p>
-                <p className="mb-5">Already have an account? <Link href={'/login'}><label htmlFor="createchatbot_modal" className="btn btn-sm">Login</label></Link></p>
-                <p className="text-sm mb-1">Full Name</p>
-                <input className="input input-bordered mb-5 max-w-xs" placeholder="Full Name" type="text" onChange={(x) => setName(x.target.value)} value={name} />
-                <p className="text-sm mb-1">Email</p>
-                <input className="input input-bordered mb-5 max-w-xs" placeholder="Email" type="text" onChange={(x) => setEmail(x.target.value)} value={email} />
-                <p className="text-sm mb-1">Password</p>
-                <input className="input input-bordered mb-5 max-w-xs" placeholder="Password" type="password" onChange={(x) => setPassword(x.target.value)} value={password} />
-                {verificationCodeSent && <div className="flex flex-col">
-                    <p className="text-sm mb-1">Verification Code</p>
-                    <input className="input input-bordered mb-5 max-w-xs" placeholder="Verification Code" type="text" onChange={(x) => setVerificationCode(x.target.value)} value={verificationCode} />
-                </div>}
-                <button className="btn btn-primary max-w-xs" onClick={() => {
-                    if (loading) return;
-                    if (!verificationCodeSent) {
-                        sendVerificationCode();
-                    }
-                    else {
-                        verifyEmail();
-                    }
-                }}>{loading ? <span className="loading loading-spinner"></span> : verificationCodeSent ? "Create Account" : "Send Verification Code"}</button>
-            </div>
-            <ToastContainer />
-        </main>
-    )
+        <>
+            <Head>
+                <title>{`Sign Up - ${appName}`}</title>
+                <meta name="description" content={`Create an account on ${appName} - AI-Powered Exam Sheet Evaluator. Enjoy seamless access to effortless evaluation.`} />
+            </Head>
+            <main className="w-screen h-screen bg-base-100 flex p-2 overflow-hidden">
+                {/* Sidebar */}
+                <div className='flex flex-col text-white p-10 max-w-[30vw] bg-gradient-to-b to-purple-400 via-violet-500 from-indigo-600 h-full rounded-md'>
+                    <Link href={"/"}><p className="mb-10 text-3xl">🤖 {appName} 📝</p></Link>
+                    <p className="text-2xl font-semibold mb-2">
+                        {appName} - AI Powered Exam Sheet Evaluator
+                    </p>
+                    <p className="opacity-80">Seamless Access, Effortless Evaluation: Welcome to {appName}, Where Innovation Meets Intelligent Grading.</p>
+                </div>
+
+                {/* Sign Up Form */}
+                <div className="animate-fade-in-bottom flex flex-col w-full h-full ml-2 rounded-md p-10 overflow-y-auto">
+                    <p className="font-bold text-2xl mb-3">Sign Up</p>
+                    <p className="mb-5">Already have an account? <Link href={'/login'}><span className="btn btn-sm btn-outline">Login</span></Link></p>
+                    
+                    {/* Full Name */}
+                    <div className="flex flex-col mb-4">
+                        <label className="text-sm mb-1">Full Name</label>
+                        <input 
+                            className={`input input-bordered ${nameError ? 'input-error' : ''}`} 
+                            placeholder="Full Name" 
+                            type="text" 
+                            onChange={(e) => setName(e.target.value)} 
+                            value={name} 
+                        />
+                        {nameError && <p className="text-red-500 text-xs mt-1">{nameError}</p>}
+                    </div>
+
+                    {/* Email */}
+                    <div className="flex flex-col mb-4">
+                        <label className="text-sm mb-1">Email</label>
+                        <input 
+                            className={`input input-bordered ${emailError ? 'input-error' : ''}`} 
+                            placeholder="Email" 
+                            type="email" 
+                            onChange={(e) => setEmail(e.target.value)} 
+                            value={email} 
+                        />
+                        {emailError && <p className="text-red-500 text-xs mt-1">{emailError}</p>}
+                        <p className="text-xs text-gray-500 mt-1">Please enter a valid email address (e.g., user@example.com)</p>
+                    </div>
+
+                    {/* Password */}
+                    <div className="flex flex-col mb-4">
+                        <label className="text-sm mb-1">Password</label>
+                        <input 
+                            className={`input input-bordered ${passwordError ? 'input-error' : ''}`} 
+                            placeholder="Password" 
+                            type="password" 
+                            onChange={(e) => setPassword(e.target.value)} 
+                            value={password} 
+                        />
+                        {passwordError && <p className="text-red-500 text-xs mt-1">{passwordError}</p>}
+                        <p className="text-xs text-gray-500 mt-1">Password must be at least 8 characters long and include uppercase, lowercase letters, numbers, and special characters.</p>
+                    </div>
+
+                    {/* Verification Code */}
+                    {verificationCodeSent && (
+                        <div className="flex flex-col mb-4">
+                            <label className="text-sm mb-1">Verification Code</label>
+                            <input 
+                                className="input input-bordered" 
+                                placeholder="Verification Code" 
+                                type="text" 
+                                onChange={(e) => setVerificationCode(e.target.value)} 
+                                value={verificationCode} 
+                            />
+                        </div>
+                    )}
+
+                    {/* Buttons */}
+                    <div className="flex space-x-2">
+                        {!verificationCodeSent && (
+                            <button 
+                                onClick={sendVerificationCode} 
+                                disabled={loading} 
+                                className={`btn btn-primary ${loading ? "loading" : ""}`}
+                            >
+                                Send Verification Code
+                            </button>
+                        )}
+
+                        {verificationCodeSent && (
+                            <button 
+                                onClick={verifyEmail} 
+                                disabled={loading} 
+                                className={`btn btn-primary ${loading ? "loading" : ""}`}
+                            >
+                                Verify Email & Sign Up
+                            </button>
+                        )}
+                    </div>
+                    <ToastContainer />
+                </div>
+            </main>
+        </>
+    );
 }
