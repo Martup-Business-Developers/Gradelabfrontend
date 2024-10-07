5c71fdc56cdb75f76d2da60f4c3acb6c07835ea3
Akshat <70918043+ftaxats@users.noreply.github.com>
2024-10-07 19:41:46 +0530

    Update home.tsx

--- Projects/frontend/src/app/home/home.tsx
+++ Projects/frontend/src/app/home/home.tsx
@@ -101,7 +101,6 @@ export default function Home({
    {/* Sidebar */}
       <div className={'print flex flex-col p-5 min-w-[275px] max-w-[15vw] h-full rounded-md ' + (!showMenu ? "max-sm:hidden " : "max-sm:fixed max-sm:w-full max-sm:h-full max-sm:max-w-none bg-base-100 max-sm:z-50 ")}>
         <div className="flex justify-between items-center max-sm:mb-4">
-          <Link href="/"><div className="mb-5 font-semibold max-sm:mb-3" onClick={() => setSelectedEvaluator(-1)}>🤖 {appName} 📝</div></Link>
           <Link href="https://gradelab.io/wp-content/uploads/2024/10/GradeLab-black.png">
     <div className="mb-5 font-semibold max-sm:mb-3" onClick={() => setSelectedEvaluator(-1)}>
         🤖 {appName} 📝
