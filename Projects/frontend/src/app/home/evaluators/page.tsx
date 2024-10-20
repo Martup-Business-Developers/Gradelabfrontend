69f2ced368241b009da83e767a6d83fb47790e7b
Akshat <70918043+ftaxats@users.noreply.github.com>
2024-10-20 19:57:41 +0530

    Update page.tsx

--- Projects/frontend/src/app/home/evaluators/page.tsx
+++ Projects/frontend/src/app/home/evaluators/page.tsx
@@ -1,7 +1,6 @@
 "use client";
-
-import React, { useContext, useEffect, useRef } from "react";
-import { FiBook, FiCheck, FiFileText, FiInfo, FiKey, FiUsers, FiX } from "react-icons/fi";
+import { useContext, useEffect, useRef } from "react";
+import { FiBook, FiCheck, FiExternalLink, FiFileText, FiImage, FiInfo, FiKey, FiShoppingCart, FiUsers } from "react-icons/fi";
 import { MainContext } from "@/context/context";
 import { UploadDropzone } from "@/utils/uploadthing";
 import { toast } from "react-toastify";
@@ -16,6 +15,7 @@ export default function Evaluators() {
     setSelectedEvaluator,
     getStudents,
     students,
+    updateEvaluation,
     getEvaluation,
     answerSheets,
     setAnswerSheets,
@@ -24,9 +24,11 @@ export default function Evaluators() {
     setEvaluating,
     evaluationData,
     setImgPreviewURL,
+    imgPreviewURL,
+    limits
   } = useContext(MainContext);
 
-  const limitExceedModalRef = useRef(null);
+  const limitExceedModalRef = useRef<any | null>(null);
 
   useEffect(() => {
     getStudents(evaluators[selectedEvaluator]?.classId);
@@ -47,11 +49,13 @@ export default function Evaluators() {
         setEvaluating(-1);
         toast.error("Evaluation failed! Please try again later.");
         return;
-      } else if (val === -2) {
+      }
+      else if (val === -2) {
         setEvaluating(-1);
         limitExceedModalRef.current?.click();
         return;
-      } else if (val === -1) {
+      }
+      else if (val === -1) {
         setEvaluating(-1);
         toast.error("Evaluation failed! Please try again later.");
         return;
@@ -65,97 +69,136 @@ export default function Evaluators() {
   };
 
   return (
-    <div className="bg-gray-100 min-h-screen p-8">
-      {evaluators.length === 0 ? (
-        <div className="bg-white rounded-lg shadow-lg p-8 max-w-5xl lg:max-w-7xl mx-auto">
-          <h1 className="text-4xl font-bold text-center mb-6">🤖 {appName} 📝</h1>
-          <p className="text-center text-gray-600 mb-8">Create a new evaluator or select an existing evaluator to get started.</p>
-          <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-6">
-            <div className="bg-blue-50 rounded-lg p-6 hover:shadow-md transition duration-300">
-              <h3 className="font-semibold text-lg mb-2">🤖 AI-Powered Evaluation</h3>
-              <p className="text-sm text-gray-600">Leverage cutting-edge AI for accurate and efficient grading.</p>
-            </div>
-            <div className="bg-green-50 rounded-lg p-6 hover:shadow-md transition duration-300">
-              <h3 className="font-semibold text-lg mb-2">📊 Detailed Result Insights</h3>
-              <p className="text-sm text-gray-600">Explore detailed insights for a holistic view of student performance.</p>
-            </div>
-            <div className="bg-purple-50 rounded-lg p-6 hover:shadow-md transition duration-300">
-              <h3 className="font-semibold text-lg mb-2">👥 Effortless Class Management</h3>
-              <p className="text-sm text-gray-600">Create, organize, and add students with ease.</p>
-            </div>
+    evaluators.length === 0 ? <div className="animate-fade-in-bottom flex flex-col w-full max-sm:max-w-none">
+      <div className='select-none flex flex-col justify-center items-center w-full h-full'>
+        <p className='text-5xl font-semibold mb-2'>🤖 {appName} 📝</p>
+        <p className='text-center'>Create a new evaluator or select an existing evaluator to get started.</p>
+        <div className='flex flex-wrap justify-center mt-7'>
+          <div className='bg-base-300 rounded-lg p-4 hover:bg-base-200 max-w-xs m-2'>
+            <p className='font-semibold text-md mb-2'>🤖 AI-Powered Evaluation</p>
+            <p className='text-sm opacity-70'>Leverage cutting-edge AI for accurate and efficient grading.</p>
+          </div>
+          <div className='bg-base-300 rounded-lg p-4 hover:bg-base-200 max-w-xs m-2'>
+            <p className='font-semibold text-md mb-2'>📊 Detailed Result Insights</p>
+            <p className='text-sm opacity-70'>Explore detailed insights for a holistic view of student performance.</p>
+          </div>
+          <div className='bg-base-300 rounded-lg p-4 hover:bg-base-200 max-w-xs m-2'>
+            <p className='font-semibold text-md mb-2'>👥 Effortless Class Management</p>
+            <p className='text-sm opacity-70'>Create, organize, and add students with ease.</p>
           </div>
         </div>
-      ) : (
-        <div className="bg-white rounded-lg shadow-lg p-8 max-w-5xl lg:max-w-7xl mx-auto">
-          <div className="flex justify-between items-center mb-6">
-            <h2 className="text-2xl font-bold flex items-center">
-              <FiFileText className="mr-2" /> {evaluators[selectedEvaluator]?.title}
-            </h2>
-            <button className="md:hidden btn btn-square" onClick={() => setSelectedEvaluator(-1)}>
-              <FiX />
-            </button>
+      </div>
+    </div> : <div className="animate-fade-in-bottom flex flex-col w-full max-sm:max-w-none">
+      <div className="hidden max-sm:flex justify-end mb-3">
+        <button className="btn btn-square" onClick={() => setSelectedEvaluator(-1)}>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+        </button>
+      </div>
+      <div className="flex items-center justify-between max-w-lg">
+        <div className="flex flex-col">
+          <div className="flex items-center justify-between mb-1 mt-4 w-full max-w-lg">
+            <p className="flex items-center font-semibold text-xl"><FiFileText className="mr-2" /> {evaluators[selectedEvaluator]?.title}</p>
           </div>
-
-          <div className="flex flex-wrap items-center mb-6">
-            <p className="flex items-center text-gray-600 mr-6 mb-2">
-              <FiBook className="mr-2" /> {evaluators[selectedEvaluator]?.class?.subject}
-            </p>
-            <p className="flex items-center text-gray-600 mb-2">
-              <FiUsers className="mr-2" /> {evaluators[selectedEvaluator]?.class?.name} {evaluators[selectedEvaluator]?.class?.section}
-            </p>
-            {Object.keys(evaluationData).length && answerSheets.length >= 1 ? (
-              <Link href={"/results/" + evaluators[selectedEvaluator]?._id} className="ml-auto">
-                <button className="btn btn-primary"><FaTrophy className="mr-2" /> View Results</button>
-              </Link>
-            ) : null}
+          <div className="flex items-center">
+            <p className="flex items-center text-sm mr-5"><FiBook className="mr-2" /> {evaluators[selectedEvaluator]?.class?.subject}</p>
+            <p className="flex items-center text-sm"><FiUsers className="mr-2" /> {evaluators[selectedEvaluator]?.class?.name} {evaluators[selectedEvaluator]?.class?.section}</p>
           </div>
-
-          {/* Question Papers */}
-          <div className="mb-8">
-            <h3 className="text-xl font-semibold mb-4 flex items-center"><FiFileText className="mr-2" /> Question Paper(s)</h3>
-            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
-              {evaluators[selectedEvaluator]?.questionPapers.map((file, i) => (
-                <label key={i} htmlFor="preview_modal" onClick={() => setImgPreviewURL(file)}>
-                  <img src={file} className="w-16 h-16 object-cover rounded-lg shadow-md hover:shadow-lg transition duration-300 cursor-pointer" alt={`Question Paper ${i + 1}`} />
-                </label>
-              ))}
+        </div>
+        {Object.keys(evaluationData).length && answerSheets.length >= 1 ? <Link href={"/results/" + evaluators[selectedEvaluator]?._id}><label className="btn btn-primary"><FaTrophy /> View Results</label></Link> : ""}
+      </div> <div className="divider max-w-lg"></div>
+      <div className="overflow-y-auto">
+        <p className="flex items-center mb-2 mt-4"><FiFileText className="mr-2" /> Question Paper(s)</p>
+        <div className="flex flex-wrap">{
+          evaluators[selectedEvaluator]?.questionPapers.map((file: string, i: number) => {
+            return <label key={i} htmlFor="preview_modal" onClick={() => setImgPreviewURL(file)}><img key={i} src={file} className="cursor-pointer w-20 h-20 border object-cover rounded-md mr-2 mb-2" /></label>
+          })
+        }</div>
+        <p className="flex items-center mb-2 mt-4"><FiKey className="mr-2" /> Answer Key / Criteria</p>
+        <div className="flex flex-wrap">{
+          evaluators[selectedEvaluator]?.answerKeys.map((file: string, i: number) => {
+            return <label key={i} htmlFor="preview_modal" onClick={() => setImgPreviewURL(file)}><img key={i} src={file} className="cursor-pointer w-20 h-20 border object-cover rounded-md mr-2 mb-2" /></label>
+          })
+        }</div>
+        <p className="flex items-center mb-1 mt-4"><FiFileText className="mr-2" /> Upload answer sheets</p>
+        <div className="max-h-full max-w-lg mt-4">
+          {students?.length === 0 ? <div role="alert" className="alert shadow-lg">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            <div>
+              <h3 className="font-bold">No students in class {evaluators[selectedEvaluator]?.class?.name} {evaluators[selectedEvaluator]?.class?.section}!</h3>
+              <div className="text-xs">You need to add students to the class to evaluate their answer sheets.</div>
             </div>
-          </div>
+            <label onClick={() => window.location.href = "/home/classes"} className="btn btn-primary btn-sm">Add Students</label>
+          </div> :
+            students?.map((student: any, i: any) => (
+              <div key={i} className="flex flex-col max-w-lg mb-4">
+                <p className="flex items-center mb-1">{student?.rollNo}. {student?.name} {evaluationData[student?.rollNo] && (answerSheets[i] && answerSheets[i]?.length >= 1) ? <span className="ml-2 flex items-center text-green-500 text-sm"><FiCheck className="mr-2" /> Evaluated</span> : ""}</p>
+                {answerSheets[i] && answerSheets[i]?.length >= 1 ? <div className="flex flex-wrap">{
+                  answerSheets[i]?.map((file: string, j: number) => {
+                    return <div key={j} className="relative flex items-center justify-center">
+                      {evaluating === student?.rollNo ? <div className="bg-white p-1 rounded-full absolute flex items-center text-sm"><span className="mr-1 loading loading-spinner loading-sm"></span><p>Evaluating...</p></div> : ""}
+                      <button className="btn btn-xs btn-circle absolute right-3 top-1" onClick={() => {
+                        answerSheets[i].splice(j, 1);
+                        setAnswerSheets([...answerSheets]);
+                        updateEvaluation(evaluators[selectedEvaluator]?._id, answerSheets);
+                      }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                      </button>
+                      <label htmlFor="preview_modal" onClick={() => setImgPreviewURL(file)}><img key={j} src={file} className="cursor-pointer w-40 h-40 border object-cover rounded-md mr-2 mb-2" /></label>
+                    </div>
+                  })
+                }</div> : <UploadDropzone
+                  endpoint="media"
+                  onClientUploadComplete={(res) => {
+                    var files = [];
+                    for (const file of res) {
+                      files.push(file.url);
+                    }
 
-          {/* Answer Keys */}
-          <div className='mb-8'>
-            <h3 className='text-xl font-semibold mb-4 flex items-center'><FiKey className='mr-2' /> Answer Key / Criteria</h3>
-            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4'>
-              {evaluators[selectedEvaluator]?.answerKeys.map((file, i) => (
-                <label key={i} htmlFor='preview_modal' onClick={() => setImgPreviewURL(file)}>
-                  <img src={file} className='w-16 h-16 object-cover rounded-lg shadow-md hover:shadow-lg transition duration-300 cursor-pointer' alt={`Answer Key ${i + 1}`} />
-                </label>
-              ))}
-            </div>
-          </div>
-
-          {/* Upload Answer Sheets */}
-          <div>
-            <h3 className='text-xl font-semibold mb-4 flex items-center'><FiFileText className='mr-2' /> Upload answer sheets</h3>
-            {students?.length === 0 ? (
-              <div className='bg-blue-50 border-l-4 border-blue-500 p-4 mb-6'>
-                <div className='flex'>
-                  <FiInfo className='flex-shrink-0 h-5 w-5 text-blue-500' />
-                  <p>No students in the selected class!</p>
-                </div>
+                    answerSheets[i] = files;
+                    setAnswerSheets([...answerSheets]);
+                    updateEvaluation(evaluators[selectedEvaluator]?._id, answerSheets);
+                  }}
+                  onUploadError={(error: Error) => {
+                    alert(`ERROR! ${error.message}`);
+                  }}
+                />}
               </div>
-            ) : (
-              students?.map((student, i) => (
-                // Student Information and Upload Area
-                ...
-              ))
-            )}
-          </div>
-
-          {/* Limit Exceeded Modal */}
-          ...
+            ))
+          }
         </div>
-      )}
+      </div>
+      {evaluating !== -1 ? <div className="flex flex-col mb-5">
+        <p className="mb-2">Evaluating Answer Sheets of {students[evaluating - 1]?.name}... (Student {evaluating} of {students.length})</p>
+        <progress className="progress max-w-lg" value={evaluating} max={students.length}></progress>
+      </div> : <button className="btn btn-primary w-full max-w-lg mt-5" onClick={() => evaluateAnswerSheets()}>🤖 Evaluate</button>}
+      <div className="flex justify-center my-2 max-w-lg">
+        <p className="flex items-center text-xs opacity-70 mr-1"><FiFileText className="mr-1" /> {limits?.evaluationLimit} evaluations left</p>
+        <Link href="/shop"><button className="btn btn-xs btn-ghost"><FiShoppingCart /> SHOP</button></Link>
+      </div>
+      <input type="checkbox" id="preview_modal" className="modal-toggle" />
+      <div className="modal" role="dialog">
+        <div className="modal-box">
+          <div className="flex items-center justify-between mb-2">
+            <h3 className="text-lg font-bold flex items-center"><FiImage className="mr-2" /> Preview</h3>
+            <button className="btn btn-square" onClick={() => window.open(imgPreviewURL)}><FiExternalLink /></button>
+          </div>
+          <img src={imgPreviewURL} className="w-full h-full object-contain" />
+        </div>
+        <label className="modal-backdrop" htmlFor="preview_modal">Close</label>
+      </div>
+      {/* Evaluation Limit Exceed Modal */}
+      <input type="checkbox" id="evaluationlimitexceed_modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="flex items-center font-bold text-lg"><FiInfo className="mr-1" /> Evaluation limit exceeded</h3>
+          <p className="py-4">You have reached the maximum limit of evaluations.<br />You can purchase more evaluations from the shop.</p>
+          <div className="modal-action">
+            <label ref={(x) => limitExceedModalRef.current = x} htmlFor="evaluationlimitexceed_modal" className="btn">Cancel</label>
+            <label htmlFor="evaluationlimitexceed_modal" className="btn btn-primary" onClick={() => window.location.href = "/shop"}><FiShoppingCart /> Shop</label>
+          </div>
+        </div>
+        <label className="modal-backdrop" htmlFor="evaluationlimitexceed_modal">Cancel</label>
+      </div>
     </div>
   );
 }
