33d67960323538982065d1e71a5cb535ef8689e3
Akshat <70918043+ftaxats@users.noreply.github.com>
2025-02-04 22:33:28 +0530

    Delete Projects/frontend directory

--- Projects/frontend/src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-const serverURL = "https://api.gradelab.io"
-const hostname = "https://app.gradelab.io";
-const currencySymbol = "$";
-const appName = "GradeLab";
-//UploadThing
-const maxFileCount = 10;
-const maxFileSize = "8MB"; // 8MB
-
-export { serverURL, currencySymbol, appName, maxFileCount, maxFileSize, hostname };
